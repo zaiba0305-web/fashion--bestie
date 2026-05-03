4f6aff7085c059160283b8c34af60c08d5f05a39
zaiba0305-web <zaiba0305@gmail.com>
2026-05-03 16:23:40 +0530

    Add outfit suggestion function based on occasion

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function suggestOutfit() {
+    let occasion = document.getElementById("occasion").value;
+    let result = "";
 
+    if (occasion === "party") {
+        result = "Wear a cute dress 💃";
+    } else if (occasion === "college") {
+        result = "Jeans + top + sneakers 👟";
+    } else {
+        result = "Comfy casual outfit 😌";
+    }
+
+    document.getElementById("result").innerText = result;
+}
